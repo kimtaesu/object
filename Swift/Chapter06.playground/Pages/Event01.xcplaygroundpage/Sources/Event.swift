4c480cd4149edfb28fc71fe4bfb623018bb269c1
kimtaesu <kimtaesoo188@gmail.com>
2019-09-07 07:59:14 +0900

    Add Chapter06 event

---
+++ Swift/Chapter06.playground/Pages/Event01.xcplaygroundpage/Sources/Event.swift
@@ -0,0 +1,38 @@
+
+import Foundation
+
+public class Event {
+    let subject: String
+    let datOfWeek: Date
+    var from: Date
+    var duration: TimeInterval
+    
+    public init(subject: String, datOfWeek: Date, from: Date, duration: TimeInterval
+        ) {
+        self.subject = subject
+        self.datOfWeek = datOfWeek
+        self.from = from
+        self.duration = duration
+    }
+    
+    func isSatisfied(schedule: RecurringSchedule) -> Bool {
+        if from != schedule.dayOfWeek ||
+            from != schedule.from ||
+            duration != schedule.duration
+        {
+            reschedule(schedule: schedule)
+            return false
+        }
+        
+        return true
+    }
+    
+    private func reschedule(schedule: RecurringSchedule) {
+        self.from.addTimeInterval(daysDistance(schedule: schedule))
+        duration = schedule.duration
+    }
+    
+    private func daysDistance(schedule: RecurringSchedule) -> TimeInterval {
+        return schedule.dayOfWeek.timeIntervalSince(self.from)
+    }
+}
