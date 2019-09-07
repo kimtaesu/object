bc1895c78412ff20aaaea9a1ef293d0d435ac8c2
kimtaesu <kimtaesoo188@gmail.com>
2019-09-07 09:26:41 +0900

    Add Codes

---
+++ Swift/Chapter10.playground/Pages/Step05.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+NightlyDiscountPhone(nightlyAmount: Money.ZERO, regularAmount: Money.ZERO, seconds: 0, taxRate: 0).calculateFee()
+//: [Next](@next)
